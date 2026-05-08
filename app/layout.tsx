f6474fc2a85441738e764fd3b783ed5bb13b24ca
Barry Hillier <barryhillier1-code@gmail.com>
2026-05-08 17:07:38 -0230

    Install Google Tag Manager globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,7 @@ import { siteConfig } from '@/lib/siteConfig';
 import { Cormorant_Garamond, Manrope } from 'next/font/google';
 import './globals.css';
 
-const GA_MEASUREMENT_ID = 'PASTE_GA4_ID_HERE';
-const CLARITY_PROJECT_ID = 'PASTE_CLARITY_HERE';
+const GTM_CONTAINER_ID = 'GTM-5DZSJ24K';
 
 const manrope = Manrope({
   variable: '--font-manrope',
@@ -51,33 +50,27 @@ export default function RootLayout({
     <html lang="en-CA" className={[manrope.variable, cormorant.variable, 'h-full'].join(' ')}>
       <head>
         <Schema />
-        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`} />
         <script
           dangerouslySetInnerHTML={{
             __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_MEASUREMENT_ID}');
-            `,
-          }}
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);
-                t.async=1;
-                t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];
-                y.parentNode.insertBefore(t,y);
-              })(window, document, "clarity", "script", "${CLARITY_PROJECT_ID}");
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','${GTM_CONTAINER_ID}');
             `,
           }}
         />
       </head>
       <body className="min-h-full bg-[var(--background)] font-sans text-[var(--foreground)] antialiased">
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_CONTAINER_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         {netlifyFormDefinitions.map((definition) => (
           <form
             key={definition.name}
